1ddf5577f5f6f8c607c14adc3a702854da642806
Gustavo Ponce <gustavo.ponce@itexico.com>
2019-12-09 15:43:19 -0600

    Adding input validation

--- src/main/java/com/gpch/mongo/controller/ReservationThymeleafController.java
+++ src/main/java/com/gpch/mongo/controller/ReservationThymeleafController.java
@@ -5,11 +5,13 @@ import com.gpch.mongo.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -44,7 +46,11 @@ public class ReservationThymeleafController {
     }
 
     @PostMapping("/save-reservation")
-    public String editReservation(@ModelAttribute("reservation") Reservation reservation) {
+    public String editReservation(@ModelAttribute("reservation") @Valid Reservation reservation,
+                                  BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "add-edit";
+        }
         reservationService.saveReservation(reservation);
         return "redirect:reservations-ui";
     }
--- src/main/java/com/gpch/mongo/model/Reservation.java
+++ src/main/java/com/gpch/mongo/model/Reservation.java
@@ -8,6 +8,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -18,7 +20,9 @@ import java.time.LocalDateTime;
 public class Reservation {
     @Id
     private String id;
+    @NotBlank(message="Please provide a name.")
     private String name;
     @DateTimeFormat(pattern="dd-MM-yyyy HH:mm")
+    @NotNull(message="Please provide a date whit the format dd-MM-yyyy HH:mm")
     private LocalDateTime date;
 }
